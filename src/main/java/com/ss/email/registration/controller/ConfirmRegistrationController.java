9ce319ed5ad29304f659e25e49cc4b8879c2f89b
bnilles <joseph.nilles@substack.com>
2021-10-11 05:37:03 -0700

    add jenkinsfile and docker file edit ppom

--- src/main/java/com/ss/email/registration/controller/ConfirmRegistrationController.java
+++ src/main/java/com/ss/email/registration/controller/ConfirmRegistrationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/signup")
+@CrossOrigin
 public class ConfirmRegistrationController {
     private final AccountService accountService;
     private final LoansService loansService;
